bba64d6ac06f91e68a67ca5ef853f93d09e7ebae
Kacper Pirog <kacper.pirog@gmail.com>
2023-04-05 13:40:08 +0200

    add li todo

--- dist/app.js
+++ dist/app.js
@@ -2,3 +2,16 @@
 // <label for="task-1">śmieci</label>
 // <input type="checkbox" id="task-1" name="smieci"/>
 // </li>
+// nakramienia kota 
+//zrobic coś
+// programowac 
+const taskConteinerElement = document.querySelector(".tasks");
+const tasks = ["nakramienia kota", "zrobic coś", "programowac "];
+const render = () => {
+    tasks.forEach((task) => {
+        const taskElement = document.createElement("li");
+        taskElement.innerHTML = task;
+        taskConteinerElement.appendChild(taskElement);
+    });
+};
+render();
--- src/app.ts
+++ src/app.ts
@@ -2,3 +2,22 @@
 // <label for="task-1">śmieci</label>
 // <input type="checkbox" id="task-1" name="smieci"/>
 // </li>
+
+
+// nakramienia kota 
+//zrobic coś
+// programowac 
+
+const taskConteinerElement : HTMLElement = document.querySelector(".tasks")
+
+const tasks: string[] = ["nakramienia kota","zrobic coś", "programowac "]
+
+const render = () => {
+    tasks.forEach((task) => {
+        const taskElement: HTMLElement = document.createElement("li")
+        taskElement.innerHTML = task
+        taskConteinerElement.appendChild(taskElement)
+    })
+
+}
+render()
